395c3b57ab2fc7a24f2974c629d68a540534d46b
Twixy565 <frajohnson@my365.bellevue.edu>
2022-10-02 23:51:13 -0400

    Week 5

---
+++ Week 5.py
@@ -0,0 +1,59 @@
+# DSC 510
+# Week 5
+# Programming Assignment Week 5
+# Author Franchesca Johnson
+# 10/2/2022
+
+def Diluted_BUN(repeat):
+    print(Multiply)
+BUN = input('Value from patient sample: ')
+BUN = int(BUN)
+while int(BUN) <=24:
+    print('Results are Normal')
+    break
+if int(BUN) <50 >24:
+    print('Abnormal, Check Creatinine Result')
+elif int(BUN) >50 <94:
+    print('Needs Dilution')
+repeat = input('Value of diluted sample: ')
+repeat = int(repeat)
+def performCalculation():
+    repeat * 2
+while True:
+    print('Enter the value of repeated BUN ' +str(int(repeat) * 2))
+    break
+
+
+
+
+
+
+
+
+
+
+
+
+ #       break
+#print('The patient has normal values.')
+
+# print('Good Day')
+# print('What is your name?')  # ask for their name
+# myName = input()
+# print('It is good to meet you, ' + myName)
+# print('What company do you work for?')  # enter company name
+# companyName = input()
+# print('Can you tell me how much fiber optic is needed for this project?')  # ask how much
+# fiber = input
+# print('How much per feet')
+# if int(fiber) <=100:
+#    cost=.87
+# elif int(fiber) <= 250:
+#    cost=.80
+# elif int(fiber) <= 500:
+#    cost=.70
+# else:
+#    cost=.50
+# print(cost)
+# print('What is the total cost?')
+# print('The total cost is ' + str(int(fiber)*cost))
